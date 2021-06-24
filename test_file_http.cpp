b368a1145ec273837483800c1c0aea10ea664281
EUING <coke1324@naver.com>
2021-06-24 12:16:13 +0900

    FileHttp 한국어로 테스트

--- test_file_http.cpp
+++ test_file_http.cpp
@@ -7,12 +7,12 @@ TEST(HttpTest, AddFile) {
 	monitor_client::FileHttp file_http(L"ggulmo.iptime.org", 56380);
 
 	monitor_client::common_utility::FileInfo file_info;
-	file_info.file_name = L"name";
+	file_info.file_name = L"새 텍스트 문서.txt";
 	file_info.file_size = L"10";
 	file_info.creation_iso_time = L"2021-06-22T22:07:27";
 	file_info.last_modified_iso_time = L"2021-06-22T22:07:27";
 
-	bool result = file_http.AddedFile(file_info);
+	bool result = file_http.AddFile(file_info);
 	ASSERT_TRUE(result);
 }
 
@@ -20,21 +20,21 @@ TEST(HttpTest, ModifyFile) {
 	monitor_client::FileHttp file_http(L"ggulmo.iptime.org", 56380);
 
 	monitor_client::common_utility::FileInfo file_info;
-	file_info.file_name = L"name";
+	file_info.file_name = L"새 텍스트 문서.txt";
 	file_info.file_size = L"10";
 	file_info.creation_iso_time = L"2021-06-22T22:07:27";
 	file_info.last_modified_iso_time = L"2021-06-22T22:07:27";
 
-	bool result = file_http.ModifiedFile(file_info);
+	bool result = file_http.ModifyFile(file_info);
 	ASSERT_TRUE(result);
 }
 
 TEST(HttpTest, RemovedFile) {
 	monitor_client::FileHttp file_http(L"ggulmo.iptime.org", 56380);
 
-	std::wstring file_name = L"name";
+	std::wstring file_name = L"새 텍스트 문서.txt";
 
-	bool result = file_http.RemovedFile(file_name);
+	bool result = file_http.RemoveFile(file_name);
 	ASSERT_TRUE(result);
 }
 
@@ -42,9 +42,9 @@ TEST(HttpTest, RenamedFile) {
 	monitor_client::FileHttp file_http(L"ggulmo.iptime.org", 56380);
 
 	monitor_client::common_utility::ChangeNameInfo info;
-	info.old_name = L"old.txt";
-	info.new_name = L"new.txt";
+	info.old_name = L"새 텍스트 문서.txt";
+	info.new_name = L"새 텍스트 문서2.txt";
 
-	bool result = file_http.RenamedFile(info);
+	bool result = file_http.RenameFile(info);
 	ASSERT_TRUE(result);
 }
