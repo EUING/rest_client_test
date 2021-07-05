9f48a1736fcf81b957ceb8fc9f9858020c02a115
EUING <coke1324@naver.com>
2021-07-05 18:00:57 +0900

    HttpTest시 로컬 더미 서버를 사용하도록 수정

--- test_item_http.cpp
+++ test_item_http.cpp
@@ -1,59 +1,79 @@
 #include "pch.h"
 
+#include <Windows.h>
+
 #include <optional>
 #include <vector>
 
 #include "../monitor_client/common_utility.h"
 #include "../monitor_client/item_http.h"
 
-TEST(HttpTest, RenamedFile) {
-	monitor_client::common_utility::NetworkInfo network_info{ L"ggulmo.iptime.org", 56380 };
-	monitor_client::ItemHttp item_http(network_info);
+class HttpTest : public ::testing::Test {
+public:
+	monitor_client::ItemHttp* item_http;
+	PROCESS_INFORMATION process_info;
 
+	void SetUp() override {
+		item_http = new monitor_client::ItemHttp({ L"localhost", 8080 });
+
+		memset(&process_info, 0, sizeof(process_info));
+		
+		STARTUPINFO startup_info;
+		memset(&startup_info, 0, sizeof(startup_info));
+		startup_info.cb = sizeof(STARTUPINFO);
+		startup_info.dwFlags = STARTF_USESHOWWINDOW;
+		startup_info.wShowWindow = SW_HIDE;
+
+		wchar_t command_line[] = L"C:\\Users\\ABO\\Desktop\\repos\\monitor_dummy_server\\x64\\Release\\monitor_dummy_server.exe";
+		CreateProcess(NULL, command_line, NULL, NULL, FALSE, CREATE_NO_WINDOW, NULL, NULL, &startup_info, &process_info);
+		Sleep(1000);
+	}
+
+	void TearDown() override {
+		TerminateProcess(process_info.hProcess, 0);
+		WaitForSingleObject(process_info.hProcess, 500);
+		CloseHandle(process_info.hProcess);
+		CloseHandle(process_info.hThread);
+		delete item_http;
+	}
+};
+
+TEST_F(HttpTest, RenamedFile) {
 	monitor_client::common_utility::ChangeNameInfo info;
 	info.old_name = L"새 텍스트 문서.txt";
 	info.new_name = L"새 텍스트 문서2.txt";
 
-	bool result = item_http.RenameItem(info);
+	bool result = item_http->RenameItem(info);
 	ASSERT_TRUE(result);
 }
 
-TEST(HttpTest, AddFile) {
-	monitor_client::common_utility::NetworkInfo network_info{ L"ggulmo.iptime.org", 56380 };
-	monitor_client::ItemHttp item_http(network_info);
-
+TEST_F(HttpTest, AddFile) {	
 	monitor_client::common_utility::FileInfo file_info;
 	file_info.name = L"새 텍스트 문서.txt";
 	file_info.size = 10;
 	file_info.creation_time = L"2021-06-22T22:07:27";
 	file_info.last_modified_time = L"2021-06-22T22:07:27";
 
-	bool result = item_http.AddFile(file_info);
+	bool result = item_http->AddFile(file_info);
 	ASSERT_TRUE(result);
 }
 
-TEST(HttpTest, ModifyFile) {
-	monitor_client::common_utility::NetworkInfo network_info{ L"ggulmo.iptime.org", 56380 };
-	monitor_client::ItemHttp item_http(network_info);
-
+TEST_F(HttpTest, ModifyFile) {
 	monitor_client::common_utility::FileInfo file_info;
 	file_info.name = L"새 텍스트 문서.txt";
 	file_info.size = 10;
 	file_info.creation_time = L"2021-06-22T22:07:27";
 	file_info.last_modified_time = L"2021-06-22T22:07:27";
 
-	bool result = item_http.ModifyFile(file_info);
+	bool result = item_http->ModifyFile(file_info);
 	ASSERT_TRUE(result);
 }
 
-TEST(HttpTest, AddFolder) {
-	monitor_client::common_utility::NetworkInfo network_info{ L"ggulmo.iptime.org", 56380 };
-	monitor_client::ItemHttp item_http(network_info);
-
+TEST_F(HttpTest, AddFolder) {
 	monitor_client::common_utility::FolderInfo folder_info;
 	folder_info.name = L"new folder";
 	folder_info.creation_time = L"2021-06-29 11:30:00";
 
-	bool result = item_http.AddFolder(folder_info);
+	bool result = item_http->AddFolder(folder_info);
 	ASSERT_TRUE(result);
 }
